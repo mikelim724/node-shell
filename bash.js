8b557b64917b333a4efa36bf9811ab76a08c058e
Lihan Chen <lihan.chen49@gmail.com>
2019-09-23 11:44:06 -0500

    made ls and pwd

--- bash.js
+++ bash.js
@@ -1,8 +1,21 @@
+let pwd = require('./pwd');
+let ls = require('./ls');
+const fs = require('fs');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
+  let path = require('path');
 
   process.stdout.write('You typed: ' + cmd);
+  if (cmd === 'pwd') {
+    process.stdout.write(`\n`);
+    pwd();
+  }
+  if (cmd === 'ls') {
+    process.stdout.write(`\n`);
+    ls();
+  }
   process.stdout.write('\nprompt > ');
 });
---
+++ pwd.js
@@ -0,0 +1,15 @@
+// process.stdout.write('prompt > ');
+
+// process.stdin.on('data', data => {
+//   const cmd = data.toString().trim();
+//   let path = require('path');
+
+//   if (cmd === 'pwd') {
+//     process.stdout.write(`\n` + __dirname);
+//   }
+//   process.stdout.write('\nprompt > ');
+// });
+
+module.exports = function() {
+  process.stdout.write(__dirname);
+};
